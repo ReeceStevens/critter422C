7dbc3d77aae8f3cdb2f576a0b27432a104ed1105
ReeceStevens <reecestevens@utexas.edu>
2015-10-25 16:28:38 -0500

    fixed movement issue

---
+++ src/project4/ReeceCritter1.java
@@ -0,0 +1,19 @@
+package project4;
+
+/*
+ * Critter Description: ReeceCritter1
+ *
+ *
+ */
+
+public class ReeceCritter1 extends Critter {
+
+	@Override 
+	public String toString() { return "R"; }
+
+	@Override 
+	public void doTimeStep() {
+
+	}
+
+}
